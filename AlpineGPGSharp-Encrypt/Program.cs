6972b6f4f63a675552e46cab06970063ce75f3ce
Brooks Garrett <brooks@brooksgarrett.com>
2016-03-05 23:40:07 -0500

    Moved to sub-projects

---
+++ AlpineGPGSharp-Encrypt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlpineGPGSharp_Encrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = String.Empty;
+            StringBuilder sb = new StringBuilder();
+            while ((s = Console.ReadLine()) != null)
+            {
+                sb.AppendLine(s);
+            }
+            File.AppendAllText(Path.GetTempPath() + "\alpine_debug.txt", sb.ToString());
+        }
+    }
+}
